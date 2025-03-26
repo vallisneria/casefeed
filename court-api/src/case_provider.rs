c4ad855f5897d2a4139545af5d362be2605d859f
vallisneria <moogi3469@gmail.com>
2025-03-26 17:29:55 +0900

    `CaseProvider` 열거형의 기본값을 `CaseProvider::Casenote`로 변경함. 현재 상황에서 공식 사이트로는 `HasUrl` trait을 구현할 수 없기 때문임.

--- court-api/src/case_provider.rs
+++ court-api/src/case_provider.rs
@@ -5,10 +5,10 @@ pub enum CaseProvider {
     /// 공식 사이트
     /// - 법원의 경우 [사법정보포털](https://portal.scourt.go.kr)
     /// - 헌법재판소의 경우 [지능형 통합검색](https://isearch.ccourt.go.kr)
-    #[default]
     Official,
 
     /// 케이스노트: https://casenote.kr
+    #[default]
     Casenote,
 
     /// 엘박스: https://lbox.kr
